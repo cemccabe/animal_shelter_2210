5c36e995d5a6db39aa97b139b29e4e4bde36d950
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-12 09:38:32 -0600

    Pass fourth I1 test

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,9 +1,11 @@
 class Shelter
     attr_reader :name
     attr_reader :capacity
+    attr_reader :pets
 
     def initialize(name, capacity)
         @name = name
         @capacity = capacity
+        @pets = []
     end
 end
